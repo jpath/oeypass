b0f7852e2314849df8f9528f499c02e415060cd7
James Hughes <hughes.james@gmail.com>
2011-11-03 16:06:31 -0400

    Added setup for DataMapper.

--- app.rb
+++ app.rb
@@ -3,6 +3,21 @@ require 'sinatra'
 require 'data_mapper'
 require 'haml'
 
+# MODELS
+class Student
+  include DataMapper::Resource
+  property :id,         Serial
+  property :name,       String
+  property :email,      String
+  property :pass_type,  String
+  property :quantity,   String
+end
+
+# CONFIGURATION
+configure :development do
+  DataMapper.setup(:default, 'sqlite3://home/jhughes/dev/oeypass/db/development.db')
+end
+
 get '/' do
   @students = Student.find(:all)
   haml :index
@@ -15,14 +30,5 @@ end
 post '/pass' do
   puts params
   Student.save
-end
-
-class Student
-  include DataMapper::Resource
-  property :id,       Serial
-  property :name,     String
-  property :email,     String
-  property :pass_type, String
-  property :quantity, String
 end
 
