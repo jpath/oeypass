eecf5621d946bb7646b9cda3c01f6f8b0c40abb5
James Hughes <hughes.james@gmail.com>
2011-11-08 16:58:07 -0500

    JS for showing/hiding pass type selectors.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-  // do stuff when DOM is ready
+  $("input#monthly_radio").click(function() {
+    $("div#class_package_select").hide();
+    $("div#monthly_select").show();
+  })
+  $("input#class_package_radio").click(function() {
+    $("div#monthly_select").hide();
+    $("div#class_package_select").show();
+  })
 });
