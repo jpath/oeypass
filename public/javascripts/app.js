adbd75727af9ac65fb7b432002d645b7e693ad6e
James Hughes <hughes.james@gmail.com>
2011-11-25 13:40:50 -0500

    Add student options to student intake form

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  $("input#monthly_radio").click(function() {
+  $("input.monthly_radio").click(function() {
     $("div#class_package_select").hide();
     $("div#monthly_select").show();
   })
-  $("input#class_package_radio").click(function() {
+  $("input.class_package_radio").click(function() {
     $("div#monthly_select").hide();
     $("div#class_package_select").show();
   })
