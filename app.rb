992beae1625e07d31a39b3db2b44725b6500fd71
James Hughes <hughes.james@gmail.com>
2012-03-10 13:58:44 -0500

    Only wait 3 minutes for today? method.

--- app.rb
+++ app.rb
@@ -47,7 +47,8 @@ class Visit
   belongs_to :student
 
   def today?
-    Time.now - created_at < 3.hours
+    Time.now - created_at < 3.minutes
+    #Time.now - created_at < 3.hours
     #false
   end
 end
